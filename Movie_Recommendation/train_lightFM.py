7fd7f3e6989fe40546040d2b3a86ad5fa1269c4c
yennanliue <f339339@gmail.com>
2017-09-21 00:10:31 +0100

    add model tune function

--- Movie_Recommendation/train_lightFM.py
+++ Movie_Recommendation/train_lightFM.py
@@ -10,6 +10,31 @@ from lightfm.evaluation import precision_at_k
 from feature import *
 
 
+def tune():
+	# import data 
+	data = fetch_movielens(min_rating=5.0)
+
+	alpha_ = [1e-05,1.5e-05,2e-05]
+	epochs_ = [50,60,70]
+	num_components_ = [30,32,34]
+
+	for alpha in alpha_:
+		for epochs in epochs_:
+			for num_components in num_components_:
+				warp_model = LightFM(no_components=num_components,
+				                    loss='warp',
+				                    learning_schedule='adagrad',
+				                    max_sampled=100,
+				                    user_alpha=alpha,
+				                    item_alpha=alpha)
+				warp_model.fit(data['train'], epochs=epochs, num_threads=2)
+				print ('alpha = ', alpha, \
+					   'epochs = ', epochs, \
+					   'num_components = ', num_components )
+				print("Train precision: %.2f" % precision_at_k(warp_model, data['train'], k=5).mean())
+				print("Test precision: %.2f" % precision_at_k(warp_model, data['test'], k=5).mean())
+
+
 
 def train():
 	# import data 
@@ -65,8 +90,9 @@ def sample_recommendation(model, data, user_ids):
 
 
 if __name__ == '__main__':
-	model, data  = train()
-	sample_recommendation(model, data, [0,1,2,3,4,13, 25, 450])
+	#model, data  = train()
+	#sample_recommendation(model, data, [0,1,2,3,4,13, 25, 450])
+	tune()
 
 
 
