5191bebb2d6d5931feb17208607bb9267619ffd4
yennanliue <f339339@gmail.com>
2018-03-16 16:20:53 +0000

    modify all installation route to dynamic user define route

--- install_pyspark.sh
+++ install_pyspark.sh
@@ -13,13 +13,17 @@ Process :
 1) download spark bin-hadoop main file  (make sure you have Java 8 or newer version) 
 2) unzip its and move to the ops location (default as /Users/yennanliu/spark )
 3) copy all files under /Users/yennanliu/spark-2.3.0-bin-hadoop2.7  to /Users/yennanliu/spark
-3) install python spark API library : pyspark
-4) declare env parameter
-5) run the spark via command : pysark 
+4) install python spark API library : pyspark
+5) declare env parameter
+6) run the spark via command : pysark 
 
 #-------------------
 
 COMMENT1
+
+# get ops route 
+my_route=$(PWD)
+echo $my_route
 
 # set dev env 
 conda create -n pyspark_ python=3.5
@@ -31,17 +35,20 @@ source activate pyspark_
 cd Downloads
 wget --quiet http://apache.mirror.anlx.net/spark/spark-2.3.0/spark-2.3.0-bin-hadoop2.7.tgz
 tar -xzf spark-2.3.0-bin-hadoop2.7.tgz
-mv spark-2.3.0-bin-hadoop2.7 /Users/yennanliu/spark-2.3.0-bin-hadoop2.7
+#mv spark-2.3.0-bin-hadoop2.7 /Users/yennanliu/spark-2.3.0-bin-hadoop2.7
+mv spark-2.3.0-bin-hadoop2.7 $my_route/spark-2.3.0-bin-hadoop2.7
 
 cd ~
 #mkdir spark
-cp -R /Users/yennanliu/spark-2.3.0-bin-hadoop2.7 /Users/yennanliu/spark
+#cp -R /Users/yennanliu/spark-2.3.0-bin-hadoop2.7 /Users/yennanliu/spark
+cp -R $my_route/spark-2.3.0-bin-hadoop2.7 $my_route/spark
 
 # install python pyspark library 
 pip install pyspark
 
 # declare env parameter
-export SPARK_HOME=/Users/yennanliu/spark
+#export SPARK_HOME=/Users/yennanliu/spark
+export SPARK_HOME=$my_route/spark
 export PATH=$SPARK_HOME/bin:$PATH
 
 
