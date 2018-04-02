38d2fa2532713647e54eaa4ee136ad1f82dbac92
yennanliue <f339339@gmail.com>
2018-04-02 15:30:49 +0100

    finish load full data set logic

--- movie_recommend_spark.py
+++ movie_recommend_spark.py
@@ -42,10 +42,11 @@ def get_data_preview():
 
 
 def get_data(full_dataset=False):
+
+	datasets_path = '/Users/yennanliu/movie_recommendation/datasets/'
 	if full_dataset==False:
-		# rating small dataset 
+		#------  rating small dataset  ------#
 		# userid, movieid, rating, timestamp 
-		datasets_path = '/Users/yennanliu/movie_recommendation/datasets/'
 		small_ratings_file = os.path.join(datasets_path, 'ml-latest-small', 'ratings.csv')
 		small_ratings_raw_data = sc.textFile(small_ratings_file)
 		# get heater 
@@ -54,7 +55,7 @@ def get_data(full_dataset=False):
 		# only get 1st, 2rd, and 3rd columns
 		small_ratings_data = small_ratings_raw_data.filter(lambda line: line!=small_ratings_raw_data_header)\
 		.map(lambda line: line.split(",")).map(lambda tokens: (tokens[0],tokens[1],tokens[2])).cache()
-		# movie dataset 
+		#------ movie dataset ------#
 		# movieid, name 
 		small_movies_file = os.path.join(datasets_path, 'ml-latest-small', 'movies.csv')
 		small_movies_raw_data = sc.textFile(small_movies_file)
@@ -65,14 +66,22 @@ def get_data(full_dataset=False):
 		return small_ratings_data, small_movies_data
 
 	elif full_dataset==True:
-		# Load the complete dataset file
+		#------ rating completed dataset  ------# 
 		complete_ratings_file = os.path.join(datasets_path, 'ml-latest', 'ratings.csv')
 		complete_ratings_raw_data = sc.textFile(complete_ratings_file)
 		complete_ratings_raw_data_header = complete_ratings_raw_data.take(1)[0]
-
-		# Parse
+		# filter out header 
 		complete_ratings_data = complete_ratings_raw_data.filter(lambda line: line!=complete_ratings_raw_data_header)\
 		    .map(lambda line: line.split(",")).map(lambda tokens: (int(tokens[0]),int(tokens[1]),float(tokens[2]))).cache()
+		#------ movie completed dataset  ------# 
+		complete_movies_file = os.path.join(datasets_path, 'ml-latest', 'movies.csv')
+		complete_movies_raw_data = sc.textFile(complete_movies_file)
+		complete_movies_raw_data_header = complete_movies_raw_data.take(1)[0]
+		# filter out header 
+		complete_movies_data = complete_movies_raw_data.filter(lambda line: line!=complete_movies_raw_data_header)\
+		    .map(lambda line: line.split(",")).map(lambda tokens: (int(tokens[0]),tokens[1],tokens[2])).cache()
+		complete_movies_titles = complete_movies_data.map(lambda x: (int(x[0]),x[1]))
+		return complete_ratings_data, complete_movies_data
 
  
 
@@ -148,6 +157,8 @@ if __name__ == '__main__':
 	print ('************')
 	ALS_model_predict(model,test_for_predict_RDD,test_RDD)
 	print ('************')
+	#complete_ratings_data, complete_movies_data = get_data(full_dataset=True)
+	#print(complete_ratings_data.take(3))
 
 
 
