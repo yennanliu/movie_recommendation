6f0f345304b0b6af9e2d15ff41b69ce0039c0933
yennanliue <f339339@gmail.com>
2018-05-19 15:11:48 +0100

    standardize user-movie matrix

--- movie_recommend_KNN.py
+++ movie_recommend_KNN.py
@@ -46,16 +46,24 @@ def get_train_test_data(df):
 def get_user_movie_metrix(df):
 	cols = ['userId', 'movieId', 'rating']
 	df_ = df[cols]
-	df_ratings_grouped = df_.groupby(['userId', 'movieId']).sum()#.reset_index()
+	df_ratings_grouped = df_.groupby(['userId', 'movieId']).sum()
 	df_ratings_pivot = pd.pivot_table(df_ratings_grouped,columns=['movieId'],index=['userId'],aggfunc=np.sum)
 	# approach 1 : fill n/a data with 0 
 	df_ratings_pivot_ = df_ratings_pivot.fillna(0)
 	df_ratings_pivot_.columns= list(set(df_.movieId))
 	df_ratings_pivot_.reset_index()
-	# approach 2 : fill n/a data mean 
+	# approach 2 : fill n/a data with mean 
 	#df_ratings_pivot_ = df_ratings_pivot.fillna(0)
+	
+	X = df_ratings_pivot_.iloc[:,1:].fillna(0)
+	df_ratings_pivot_std = df_ratings_pivot_.copy()
+	# standardize 
+	for i in X:
+		df_ratings_pivot_std[i] = preprocessing.scale(df_ratings_pivot_std[i])
+	
 	print (df_ratings_pivot_)
-	return df_ratings_pivot_
+	print (df_ratings_pivot_std)
+	return df_ratings_pivot_,df_ratings_pivot_std
 
 
 # -------------------------------------
