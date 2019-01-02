97c987c492af6d348fee644cabc05c4f3ab190df
yennanliue <f339339@gmail.com>
2019-01-02 07:30:17 +0000

    add group by cast.. SQL command demo

--- analysis/movie_data_EDA.java
+++ analysis/movie_data_EDA.java
@@ -10,6 +10,9 @@ import static org.apache.spark.sql.functions.max;
 
 public class movie_data_EDA {
 
+	private static final String  RATING_MIDPOINT = "rating";
+    private static final String TIMESTAMP_MIDPOINT = "timestamp";
+
 	public static void main(String[] args) throws Exception {
 
 		Logger.getLogger("org").setLevel(Level.ERROR);
@@ -21,6 +24,26 @@ public class movie_data_EDA {
 
         System.out.println("=== Print out schema ===");
         responses.printSchema();
+
+        System.out.println("=== Print 20 records of responses table ===");
+        responses.show(20);
+
+        System.out.println("=== Print the movieId and rating columns of gender table ===");
+        responses.select(col("movieId"),  col("rating")).show();
+
+        System.out.println("=== Print records where the userId is equal 1 ===");
+        responses.filter(col("userId").equalTo("1")).show();
+
+        System.out.println("=== Print the count of userId ===");
+        RelationalGroupedDataset groupedDataset = responses.groupBy(col("userId"));
+        groupedDataset.count().show();
+
+        System.out.println("=== Cast the salary mid point and age mid point to integer ===");
+        Dataset<Row> castedResponse = responses.withColumn(RATING_MIDPOINT, col(RATING_MIDPOINT).cast("integer"))
+                                               .withColumn(TIMESTAMP_MIDPOINT, col(TIMESTAMP_MIDPOINT).cast("integer"));
+
+        System.out.println("=== Print out casted schema ===");
+        castedResponse.printSchema();
 
 
 
