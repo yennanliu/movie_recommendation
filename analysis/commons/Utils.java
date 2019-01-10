50e7239139712a2c2858f11b3aaa7eeaeaf51480
yennanliue <f339339@gmail.com>
2019-01-10 09:08:25 +0000

    add help utility java script

---
+++ analysis/commons/Utils.java
@@ -0,0 +1,10 @@
+package commons;
+
+public class Utils {
+
+    private Utils(){
+    };
+
+    // a regular expression which matches commas but not commas within double quotations
+    public static final String COMMA_DELIMITER = ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)";
+}
